be963076ebaf37e6f7fb0f4c8eb9a2afbc9d895d
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2024-02-24 13:16:54 +0000

    Fixed slight input lag

--- K-Engine/src/K-Engine/Player.cpp
+++ K-Engine/src/K-Engine/Player.cpp
@@ -104,6 +104,8 @@ namespace K
 			*this->direction = K::Vector3(0.0f, 0.0f, 0.0f);
 		}
 
+		*(this->parent->GetTransform()->position) += this->direction;
+
 		if (this->isJumping)
 		{
 			this->animator->PlayAnimation(2, this->sprite, false);
@@ -174,6 +176,13 @@ namespace K
 			}
 		}
 
+		if (this->isJumping)
+		{
+			this->col->ResetVelocity();
+			*(this->parent->GetTransform()->position) += K::Vector3(0.0f, 0.0f, (-(this->jumpTime - 0.5f) + 0.5f) * 1.0f);
+			this->jumpTime += K::Time::deltaTime() * 2.0f;
+		}
+
 		if (this->col->GetPosition()->z - (this->col->GetHeight() * 0.5f) - this->col->GetRadius() < -7.0f) 
 		{
 			K::Editor::Delete(this->parent);
@@ -194,12 +203,6 @@ namespace K
 
 	void Player::Bind() 
 	{
-		if (this->isJumping)
-		{
-			this->col->ResetVelocity();
-			*(this->parent->GetTransform()->position) += K::Vector3(0.0f, 0.0f, (-(this->jumpTime - 0.5f) + 0.5f) * 1.0f);
-			this->jumpTime += K::Time::deltaTime() * 2.0f;
-		}
-		*(this->parent->GetTransform()->position) += this->direction;
+		
 	}
 }
