57c685f8ca3b4164eb2a94f2f886759927c39eac
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2024-05-14 08:34:05 +0100

    Removed imfilebrowser

--- K-Engine/src/K-Engine/build.h
+++ K-Engine/src/K-Engine/build.h
@@ -13,6 +13,7 @@
 #include <unordered_map>
 #include <format>
 #include <thread>
+#include <Windows.h>
 #include "resource.h"
 
 //ImGUI
@@ -20,7 +21,6 @@
 #include "imgui_stdlib.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include "imfilebrowser.h"
 
 #ifdef K_BUILD_DLL
 	#define K_API __declspec(dllexport)
