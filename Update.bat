8629750bb01f58f7e151f77281e8fe76959cb285
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2024-12-10 09:23:15 +0000

    Trying to configure project with Cmake

--- Update.bat
+++ Update.bat
@@ -1,3 +1,3 @@
-RD /S /Q "%K-Engine%\Release-x64\assets"
-XCOPY "%K-Engine%\K-Engine\assets" "%K-Engine%\Release-x64\assets" /I /E /Y /EXCLUDE:%K-Engine%\K-Engine\assets\ignore\excludeFromBuild.txt
+RD /S /Q "%K-Engine%\Release\assets"
+XCOPY "%K-Engine%\K-Engine\assets" "%K-Engine%\Release\assets" /I /E /Y /EXCLUDE:%K-Engine%\K-Engine\assets\ignore\excludeFromBuild.txt
 pause
