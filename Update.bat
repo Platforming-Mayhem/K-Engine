b5b1e43f586dd6c74547b11fa7f818e53fcef6a1
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2024-12-10 10:27:14 +0000

    Removed dependency on environment variable

--- Update.bat
+++ Update.bat
@@ -1,3 +1,3 @@
-RD /S /Q "%K-Engine%\Release\assets"
-XCOPY "%K-Engine%\K-Engine\assets" "%K-Engine%\Release\assets" /I /E /Y /EXCLUDE:%K-Engine%\K-Engine\assets\ignore\excludeFromBuild.txt
+RD /S /Q "Release\assets"
+XCOPY "K-Engine\assets" "Release\assets" /I /E /Y /EXCLUDE:K-Engine\assets\ignore\excludeFromBuild.txt
 pause
