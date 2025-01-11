0aa2ed30525d9b785dbf1ae2769c3d0197b30805
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2025-01-11 13:25:13 +0000

    Runtime Linking should work on Linux now :)

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -1,19 +1,36 @@
 #include <K_Engine.h>
-#include <Windows.h>
+#ifdef _WIN32 || _WIN64
+	#include <Windows.h>
+#elif __unix__
+	#include <dlfcn.h>
+#endif
 
 void main(int argc, char** argv)
 {
 	K::Application* app = new K::Application();
 
+	#ifdef _WIN32 || _WIN64
 	HINSTANCE hInst = LoadLibrary(TEXT("Components.dll"));
 
-	if (hInst != NULL) 
+	if (hInst != NULL)
 	{
 		std::cout << "Found Library" << std::endl;
 	}
+	#elif __unix__
+	void* handle = dlopen("Components.so", RTLD_NOW)
+	if (handle) 
+	{
+		std::cout << "Found Library" << std::endl;
+	}
+	#endif
 
 	std::string name = argv[0];
 	app->Run(name.substr(name.find_last_of('\\') + 1).c_str());
+
+	#ifdef _WIN32 || _WIN64
 	FreeLibrary(hInst);
+	#elif __unix__
+	dlclose(handle);
+	#endif
 	delete app;
 }
