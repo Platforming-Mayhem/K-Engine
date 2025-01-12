82733c5b9c12d1ee77f0f8602fda2617b68b6e1e
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2025-01-12 17:05:34 +0000

    Update Editor.cpp

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -1,5 +1,5 @@
 #include <K_Engine.h>
-#ifdef _WIN32 || _WIN64
+#ifdef _WIN32
 	#include <Windows.h>
 #elif __unix__
 	#include <dlfcn.h>
@@ -9,7 +9,7 @@ void main(int argc, char** argv)
 {
 	K::Application* app = new K::Application();
 
-	#ifdef _WIN32 || _WIN64
+	#ifdef _WIN32
 	HINSTANCE hInst = LoadLibrary(TEXT("Components.dll"));
 
 	if (hInst != NULL)
@@ -27,7 +27,7 @@ void main(int argc, char** argv)
 	std::string name = argv[0];
 	app->Run(name.substr(name.find_last_of('\\') + 1).c_str());
 
-	#ifdef _WIN32 || _WIN64
+	#ifdef _WIN32
 	FreeLibrary(hInst);
 	#elif __unix__
 	dlclose(handle);
