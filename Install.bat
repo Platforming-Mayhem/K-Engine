12b56fd27b3e6719c15753329feaeecfaaf3ad2d
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2025-03-28 02:20:12 +0000

    Install now creates a desktop shortcut

--- Install.bat
+++ Install.bat
@@ -1,3 +1,12 @@
+cd /d %~dp0
 cmake -B bin
+call "%ProgramFiles%\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat" && cd bin && msbuild Editor.sln
 pause
-"%ProgramFiles%\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat" && cd bin && msbuild Editor.sln
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\Editor.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%cd%\\Debug\\Editor.exe" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = "%cd%\\Debug" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
