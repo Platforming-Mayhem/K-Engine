133cd352f52135e038132cabbb3e770699523b3a
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2025-07-22 00:07:52 +0100

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -4,7 +4,7 @@ pause
 cmake --build bin --target Editor --config Debug
 pause
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\Editor.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\K-Engine.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%cd%\\bin\\Debug\\Editor.exe" >> CreateShortcut.vbs
 echo oLink.WorkingDirectory = "%cd%\\bin\\Debug" >> CreateShortcut.vbs
