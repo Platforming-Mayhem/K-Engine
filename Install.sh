152570bc6af82e61d30396f102ebe6d2865383e5
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-07-26 12:51:16 +0100

    added -D GLFW_BUILD_WAYLAND=0 to cmake
    todo:
    - move to CMakeLists.txt instead

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-cmake -B bin/Debug
+cmake -B bin/Debug -D GLFW_BUILD_WAYLAND=0
 read -p "Press any key to resume ..."
 cmake --build bin/Debug --target Editor
 read -p "Press any key to resume ..."
