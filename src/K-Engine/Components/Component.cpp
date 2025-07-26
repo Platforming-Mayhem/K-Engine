7d22c0fc2ad75c9d70834dec0d511f51ae039f2a
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-07-26 16:16:39 +0100

    check unix before demangling

--- src/K-Engine/Components/Component.cpp
+++ src/K-Engine/Components/Component.cpp
@@ -9,10 +9,15 @@ namespace K
 
 	const char* Component::GetName() 
 	{
+		#if __unix__
 		int status;
 		std::string tempName = typeid(*this).name();
 		std::string demangledName = std::string("class ") + abi::__cxa_demangle(tempName.c_str(), NULL, NULL, &status);
 		return demangledName.c_str();
+		
+		#else
+		return typeid(*this).name();
+		#endif
 	}
 
 	Component::~Component() 
