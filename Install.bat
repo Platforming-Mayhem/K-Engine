b9a16f1abd3af6eefede5babd9ffc3b76fe2a179
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2025-12-30 01:28:29 +0000

    Windows Install bat now adds icon to shortcut

--- Install.bat
+++ Install.bat
@@ -7,6 +7,7 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\K-Engine.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%cd%\\bin\\Debug\\Editor.exe" >> CreateShortcut.vbs
+echo oLink.IconLocation = "%cd%\\assets\\textures\\watermark\\watermark.ico" >> CreateShortcut.vbs
 echo oLink.WorkingDirectory = "%cd%\\bin\\Debug" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
