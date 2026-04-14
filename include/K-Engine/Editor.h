1de98dd8990ed9d37d24748b882668660101bce8
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2026-04-14 05:30:26 +0100

    This needs to be a pointer

--- include/K-Engine/Editor.h
+++ include/K-Engine/Editor.h
@@ -132,7 +132,7 @@ namespace K
 	{
 		Register()
 		{
-			K::IFactory tempFactory = K::Factory<T>();
+			K::IFactory* tempFactory = new K::Factory<T>;
 			std::string tempName = typeid(T).name();
 
 			#if __unix__
@@ -141,7 +141,7 @@ namespace K
 
 			std::cout << demangledName << std::endl;
 
-			K::Editor::lst().insert({ demangledName, &tempFactory });
+			K::Editor::lst().insert({ demangledName, tempFactory });
 			#else
 			K::Editor::lst().insert({ tempName, tempFactory });
 			#endif
