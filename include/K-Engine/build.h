842e60167417555b2bcda1e301319d3b1a782f1c
PM <82027284+Platforming-Mayhem@users.noreply.github.com>
2026-07-08 12:56:14 +0100

    Removed Assimp from build.h

--- include/K-Engine/build.h
+++ include/K-Engine/build.h
@@ -32,11 +32,6 @@
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 
-//ASSIMP
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
 //STB
 #include <stb_image.h>
 
